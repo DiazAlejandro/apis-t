58cc9ce02c8d68166fd760709f93e565e3d4b268
Alejandro Diaz <alejandro_diaz1253@hotmail.com>
2021-12-22 21:06:02 -0600

    Corrección parapoder inscribir a un curso

--- alumno/controllers/Inscribir_curso.php
+++ alumno/controllers/Inscribir_curso.php
@@ -5,7 +5,7 @@
         $clave = $_GET['clave'];
         $curpA = "";
         //Obtener curp del alumno
-        $alumno = mysqli_query($conexion,"SELECT curp FROM alumno WHERE email='".$_SESSION['correo']."'");
+        $alumno = mysqli_query($conexion,"SELECT curp FROM alumno WHERE email='".$_SESSION['email']."'");
         if (mysqli_num_rows($alumno) > 0) {
         while ($alf = mysqli_fetch_assoc($alumno)) {
             $curpA = $alf['curp'];
@@ -26,11 +26,11 @@
         //echo $clave." ".$curpA." ".$fecha_actual." ".$duracionC;
         //echo $fecha_final;
 
-        //$folio=mt_rand(1,99999);
+        $folio=mt_rand(1,99999);
 
         $sqlInscribir = "INSERT INTO `inscripcion`(`folio`, `fecha_inicio`, `fecha_fin`, `alumno_curp`, `curso_clave`) 
                     VALUES ('$folio','$fecha_actual','$fecha_final','$curpA','$clave')";
-                    echo $sqlInscribir;
+                    //echo $sqlInscribir;
 
         if (mysqli_query($conexion,$sqlInscribir)){
             $messaget = "INSCRIPCIÓN REALIZADA CORRECTAMENTE";
