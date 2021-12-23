d769ba35b07de07c41638e8a87edc8d2eba5149a
OscarLS <l.s.oscar.2017@gmail.com>
2021-12-22 19:00:31 -0600

    tabla alumnos

--- recepcionista/inicio.php
+++ recepcionista/inicio.php
@@ -9,91 +9,74 @@
     }
 ?>
 
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="es">
-<head>
-    <link rel="shortcut icon" href="../img/logo-header.png">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="../css/recep.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;900&display=swap"
-        rel="stylesheet">
-        
-    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-    <!--Título de la pagina-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    
     <title>Inicio - Recepcionista</title>
 </head>
+
 <body id="fondo">
-    <!--Barra de navegación-->
-<nav class="navbar navbar-expand-lg  ">
+<!-- Barra de navegación-->
+    <nav class="navbar navbar-expand-lg  ">
         <div class="container">
-            <div class="form-group logo-img " >
+            <div class="form-group logo-img ">
                 <img src="../img/logo-header.png" width="80" height="80">
             </div>
             <div class="container-fluid">
-            <a class="navbar-brand font-weight-bold lead ">INSTITUTO APIS-T</a>
+                <a class="navbar-brand" id="texto-nav">INSTITUTO APIS-T</a>
             </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon "></span>
-            </button>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto text-center">
                     <li class="nav-item">
-                        <a class="btn font-weight-bold" href="../connect/cerrar_sesion.php" id="entrar">Cerrar sesión</a>
+                        <a class="nav-link font-weight-bold" href="../connect/cerrar_sesion.php" id="entrar">Cerrar sesión</a>
                     </li>
-                 </ul>
+                </ul>
             </div>
         </div>
     </nav>
-    <div class="font-weight-bold card-header" id="barrita"></div>
- <!--Contenido
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Inicio</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="tabla_curso.php">Cursos Registrados</a>
-                <a class="nav-link active" aria-current="page" href="tabla_instructor.php">Instructores Registrados</a>
-                <a class="nav-link active" aria-current="page" href="registro_curso.php">Alta Curso</a>
-                <a class="nav-link active" aria-current="page" href="registro_instructor.php">Alta Instructores</a>
-
-            </div>
-            </div>
-        </div>
-    </nav>
--->
-<div class="wrapper">
-    <div class="sidebar">
-        <h2>Administrador</h2>
-        <ul>
-            <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
-            <li><a href="registro_instructor.php"><i class="fas fa-user"></i>Alta de instructor</a></li>
-            <li><a href="tabla_instructor.php"><i class="fas fa-address-card"></i>Instructores registrados</a></li>
-            <li><a href="registro_curso.php"><i class="fas fa-project-diagram"></i>Alta de curso</a></li>
-            <li><a href="tabla_curso.php"><i class="fas fa-blog"></i>Cursos registrados</a></li>
-            <li><a href="#"><i class="fas fa-address-book"></i>Contact</a></li>
-            <li><a href="#"><i class="fas fa-map-pin"></i>Map</a></li>
-        </ul> 
-        <div class="social_media">
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-      </div>
-    </div>
+    <nav class="navbar navbar-expand-lg" id="barrita">
+          <button class="navbar-toggler order-last" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse order-last" id="collapsibleNavbar">
+              <div>
+                  <ul class="tabs navbar-nav">
+                  <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link active text-light font-weight-bold" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link active text-light font-weight-bold" href="registro_instructor.php">Alta de instructor</a>
+                    </li>
+                    <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link text-light font-weight-bold" href="tabla_instructor.php">Instructores registrados</a>
+                    </li>
+                    <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link text-light font-weight-bold" href="registro_curso.php">Alta de curso</a>
+                    </li>
+                    <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link text-light font-weight-bold" href="tabla_curso.php">Cursos registrados</a>
+                    </li>
+                    <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link text-light font-weight-bold" href="reg_pagos.php">Registro de pagos</a>
+                    </li>
+                    
+                    <li class="nav-item" style="border: 1px solid white">
+                        <a class="nav-link text-light font-weight-bold" href="tabla_alumno.php">Alumnos registrados</a>
+                    </li>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
 </div>
 </body>
 </html>
