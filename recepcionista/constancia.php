dce0cf59fcd174978f7e707435c4b0a3c4d70a56
Alejandro Diaz <alejandro_diaz1253@hotmail.com>
2022-01-04 21:40:29 -0600

    Generación de constancias

--- recepcionista/constancia.php
+++ recepcionista/constancia.php
@@ -26,17 +26,44 @@
     }
     $fila = mysqli_fetch_assoc($resultado);
 
-    $mpdf = new \Mpdf\Mpdf();
-    $mpdf->WriteHTML('<div style="align-items: center; text-align: center;"> 
-    <p>El Instituto APIS-T <br>
-        Otorga la presente <br>
-        </p>
-        <h3>CONSTANCIA</h3> 
-        <p>A: '.$fila['nameAlumno'].' '.$fila['apellido_p'].' '.$fila['apellido_m'].'<br><br>
-        Por haber concluido satisfatoriamente el curso denominado: </p><br>
-        <h3>'.$fila[nombre].'</h3> <br> 
-        <p>En un periodo comprendido del '.$fila['fecha_inicio'].' al '.$fila['fecha_fin'].' <br><br>
-        Se expide la presente en la Ciudad de Oaxaca de Juárez, Oax. a los 26 días del mes de diciembre del 2021</p></div>
-   ');
+    // Obteniendo la fecha actual del sistema con PHP
+    $year = date('Y');
+    $month = date('m');
+    $day = date('d');
+
+    $mes = '';
+    if ($month == 1) $mes = 'enero';
+    if ($month == 2) $mes = 'febrero';
+    if ($month == 3) $mes = 'marzo';
+    if ($month == 4) $mes = 'abril';
+    if ($month == 5) $mes = 'mayo';
+    if ($month == 6) $mes = 'junio';
+    if ($month == 7) $mes = 'julio';
+    if ($month == 8) $mes = 'agosto';
+    if ($month == 9) $mes = 'septiembre';
+    if ($month == 10) $mes = 'octubre';
+    if ($month == 11) $mes = 'noviembre';
+    if ($month == 12) $mes = 'diciembre';
+
+
+    $mpdf = new \Mpdf\Mpdf(['orientation' => 'L']);
+    $mpdf->WriteHTML('
+
+    <body style="font-family: Arial; font-size: 15pt;">
+        <div style="align-items: center; text-align: center;"> 
+        <img src="../img/headerCons.png">
+        <p>El Instituto APIS-T otorga la presente <br>
+            </p>
+            <h3>CONSTANCIA</h3> 
+            <p>A: '.$fila['nameAlumno'].' '.$fila['apellido_p'].' '.$fila['apellido_m'].'<br><br>
+            Por haber concluido satisfatoriamente el curso denominado: </p>
+            <h3>'.$fila[nombre].'</h3>
+            <p>En un periodo comprendido del '.$fila['fecha_inicio'].' al '.$fila['fecha_fin'].' <br><br>
+            Se expide la presente en la Ciudad de Oaxaca de Juárez, Oax. a los '.$day.' días de '.$mes.' de '.$year.'</p>
+            <img src="../img/firma.png">
+            <p>Nombre de la directora <br>Directora de la Institución</p>
+            </div>
+    </body>
+    ');
     $mpdf->Output();
 ?>
