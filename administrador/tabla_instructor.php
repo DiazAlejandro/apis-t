75f3f8df562522bfa30880cf06a0e9e86b139215
Alejandro Diaz <alejandro_diaz1253@hotmail.com>
2022-01-30 00:34:15 -0600

    Corrección de colores

--- administrador/tabla_instructor.php
+++ administrador/tabla_instructor.php
@@ -170,7 +170,7 @@
                                             echo $fila['correo_electronico'];
                                         ?></td>
                                         <td style="text-align: center;">
-                                            <a href="editar_instructor.php?curp=<?php echo $fila['curp']?>" class="btn btn-secondary">
+                                            <a href="editar_instructor.php?curp=<?php echo $fila['curp']?>" class="btn btn-info">
                                                 <i class="fa fa-edit"></i>
                                             </a>
                                         </td>
