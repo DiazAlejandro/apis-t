83847304335eb0ca806dd8d0c4f6b92565e3debb
Josselyn-Aps <75766178+Josselyn-Aps@users.noreply.github.com>
2022-02-01 13:14:23 -0600

    Texto de ventanas emergentes y se agregó icono información en la tabla cumplimiento

--- administrador/controller/controller_incumplimiento.php
+++ administrador/controller/controller_incumplimiento.php
@@ -11,13 +11,13 @@
 
         $sqlInscripcion = "UPDATE inscripcion SET cumplimiento='SIN FINALIZAR' WHERE folio='$folio'";
         if (mysqli_query($conexion, $sqlInscripcion)) {
-            $messaget = "SE ACTUALIZO EL CUMPLIMIENTO DEL CURSO";
+            $messaget = "SE ACTUALIZÓ EL CUMPLIMIENTO DEL CURSO";
                 echo "<script type='text/javascript'>
                 alert('$messaget');
                 window.location.href = '../cumplimiento.php?curp=$curp&name=$name';
                 </script>";
         }else {
-            $messaget = "ERROR NO SE PUDO ACTUALIZAR EL CUMPLIMIENTO DEL CURSO";
+            $messaget = "ERROR, NO SE PUDO ACTUALIZAR EL CUMPLIMIENTO DEL CURSO";
             echo "<script type='text/javascript'>
                         alert('$messaget');
                         window.location.href = '../cumplimiento.php?curp=$curp&name=$name';
--- administrador/editar_instructor.php
+++ administrador/editar_instructor.php
@@ -121,45 +121,46 @@
                 <br><br><br>
                 <div class="card ">
                     <div class="card-header" id="cabeza">
-                        <h1 class="font-weight-bold mb-3">Editar datos del instructor</h1>
+                        <h1 class="font-weight-bold mb-3">Editar datos del Instructor</h1>
                     </div>
                     <div class="card-body">
                         <form action="controller/Instructor_update.php" method="post">
-                            <div class="form-group">
+                            <div class="form-row mb-2">
+                            <div class="form-group col-md-6">
                                 <label class="font-weight-bold">CURP:<span class="text-danger" id="marca">*</span></label>
                                 <input type="text" name="curp" class="form-control" id="curp" style="border: black 1px solid; box-shadow: 0px 10px 10px black;" value="<?php echo $curp?>">
                                 <input type="hidden" name="old_curp" class="form-control" id="old_curp" value="<?php echo $curp?>">
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label class="font-weight-bold" for="txtclave">Nombre:<span class="text-danger" id="marca">*</span></label>
                                 <input type="text" style="border: black 1px solid; box-shadow: 0px 10px 10px black;" name="nombre" class="form-control" id="nombre" value = "<?php echo $nombre?>">
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label class="font-weight-bold" >Apellido paterno: <span class="text-danger" id="marca">*</span></label>
                                 <input type="text" name="apellido_p" class="form-control" id="apellido_p" style="border: black 1px solid; box-shadow: 0px 10px 10px black;"
                                 value = "<?php echo $apellido_p?>">
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label class="font-weight-bold">Apellido materno: <span class="text-danger" id="marca">*</span></label>
                                 <input type="text" name="apellido_m" class="form-control" id="apellido_m" style="border: black 1px solid; box-shadow: 0px 10px 10px black;"
                                 value = "<?php echo $apellido_m?>">
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label class="font-weight-bold">Teléfono: <span class="text-danger" id="marca">*</span></label>
                                 <input type="text" name="telefono" class="form-control" id="telefono" style="border: black 1px solid; box-shadow: 0px 10px 10px black;"
                                 value = "<?php echo $telefono?>">
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label class="font-weight-bold">Correo electrónico <span class="text-danger" id="marca">*</span></label>
                                 <input type="email" name="email" class="form-control" id="email"  style="border: black 1px solid; box-shadow: 0px 10px 10px black;"
                                 value = "<?php echo $correo?>">
                             </div>
-
+                            </div>
                             <br>
                             <button type="submit" name="accion" value="enviar" id="btn"
                             class="btn btn-warning font-weight-bold" >Editar</button>
