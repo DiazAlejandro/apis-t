3b07b5d125aab4f1587606761c72ae1f0da3f295
Alejandro Diaz <alejandro_diaz1253@hotmail.com>
2022-02-01 14:27:56 -0600

    Borrar curso - Corrección

--- administrador/controller/Curso_delete.php
+++ administrador/controller/Curso_delete.php
@@ -3,7 +3,7 @@
     if (isset($_GET['clave'])){    
         $clave = $_GET['clave'];
         
-        $delete = "DELETE FROM curso WHERE clave = $clave";
+        $delete = "DELETE FROM curso WHERE clave = '$clave'";
         #delete from curso where clave='001'; delete from curso where clave='001';
         if (mysqli_query($conexion, $delete)){
             $messaget = "CURSO BORRADO CORRECTAMENTE";
@@ -11,8 +11,6 @@
                         alert('$messaget');
                         window.location.href = '../tabla_curso.php';
                     </script>";
-            $_SESSION['message_type'] = 'danger'; # Funcion de bootstrap
-            header('Location:../tabla_curso.php'); # Redireccionar el archivo
         }else{
             $messaget = "CURSO ACTUAL EN DESARROLLO";
                 echo "<script type='text/javascript'>
